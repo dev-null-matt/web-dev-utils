04346c94b03cfff84e2af7206cc631fb8af636e1
Matt Rick <matthew.rick2@target.com>
2015-03-30 16:19:55 -0500

    Adds additional tests with verbage changes suggested by frontend engineers

--- test/com/arrested/research/optimization/suggestion/IdBasedOptimizationSuggestionEngineTest.java
+++ test/com/arrested/research/optimization/suggestion/IdBasedOptimizationSuggestionEngineTest.java
@@ -24,6 +24,34 @@ public class IdBasedOptimizationSuggestionEngineTest {
 		Assert.assertTrue(suggestions.contains(IdBasedOptimizationSuggestionEngine.REWRITE_SELECTOR_WITH_ID_FIRST));
 	}
 	
+	@Test
+	public void suggestOptimizationsForIdSelectorTest() {
+		
+		Set<String> suggestions = engine.getSuggestions("#test .child");
+		
+		Assert.assertEquals(1, suggestions.size());
+		
+		String suggestion = suggestions.iterator().next();
+		
+		Assert.assertTrue(suggestion.indexOf("$('#test').find('.child')") > -1);
+	}
+	
+	@Test
+	public void suggestOptimizationsForIdSelectorTest_commaSeparated() {
+		Assert.assertEquals(0, engine.getSuggestions("#first, #second").size());
+	}
+	
+	@Test
+	public void suggestOptimizationsForIdSelectorTest_commaSeparated_shouldSuggest() {
+		
+		Set<String> suggestions = engine.getSuggestions("#first, #second .child");
+		
+		Assert.assertEquals(1, suggestions.size());
+
+		String suggestion = suggestions.iterator().next();
+		
+		Assert.assertTrue(suggestion.indexOf("$('#second').find('.child')") > -1);
+	}
 	
 	@Test
 	public void isIdSelectorTest_positive() {
