7e32825c4f59ea65ef196a321eb961d198b3ffc6
Matt Rick <Matthew.Rick2@target.com>
2015-04-27 13:51:45 -0500

    Adds tests for new utility method

--- src/test/java/com/arrested/research/optimization/suggestion/SelectorUtilsTest.java
+++ src/test/java/com/arrested/research/optimization/suggestion/SelectorUtilsTest.java
@@ -1,5 +1,7 @@
 package com.arrested.research.optimization.suggestion;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -18,5 +20,35 @@ public class SelectorUtilsTest {
 	@Test
 	public void isIdSelectorTest_idSelectorNotFirst() {
 		Assert.assertFalse(SelectorUtils.isIdSelector(".baz#foo"));
+	}
+	
+	@Test
+	public void tokenizeSelectorTest_happyPath() {
+		
+		List<List<String>> tokens = SelectorUtils.tokenizeSelector("#a,div.b h1,#c");
+		
+		Assert.assertEquals("Correct number of selectors", 3, tokens.size());
+		Assert.assertEquals("Correct number of tokens for selector 1", 1, tokens.get(0).size());
+		Assert.assertEquals("Correct number of tokens for selector 2", 2, tokens.get(1).size());
+		Assert.assertEquals("Correct number of tokens for selector 3", 1, tokens.get(2).size());
+		
+		Assert.assertEquals("#a", tokens.get(0).get(0));
+		Assert.assertEquals("div.b", tokens.get(1).get(0));
+		Assert.assertEquals("h1", tokens.get(1).get(1));
+		Assert.assertEquals("#c", tokens.get(2).get(0));
+	}
+	
+	@Test
+	public void tokenizeSelectorTest_sequentialSpaces() {
+		
+		List<List<String>> tokens = SelectorUtils.tokenizeSelector("#a,#b   .c");
+	
+		Assert.assertEquals("Correct number of selectors", 2, tokens.size());
+		Assert.assertEquals("Correct number of tokens for selector 1", 1, tokens.get(0).size());
+		Assert.assertEquals("Correct number of tokens for selector 2", 2, tokens.get(1).size());
+		
+		Assert.assertEquals("#a", tokens.get(0).get(0));
+		Assert.assertEquals("#b", tokens.get(1).get(0));
+		Assert.assertEquals(".c", tokens.get(1).get(1));
 	}
 }
