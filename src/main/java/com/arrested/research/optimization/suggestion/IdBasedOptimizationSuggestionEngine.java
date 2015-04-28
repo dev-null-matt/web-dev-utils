3f18ade2c74ffdd92de667c000cf7ef81866dbf6
Matt Rick <Matthew.Rick2@target.com>
2015-04-28 14:34:54 -0500

    Chains together new utils methods

--- src/main/java/com/arrested/research/optimization/suggestion/IdBasedOptimizationSuggestionEngine.java
+++ src/main/java/com/arrested/research/optimization/suggestion/IdBasedOptimizationSuggestionEngine.java
@@ -5,8 +5,8 @@ import java.util.TreeSet;
 
 class IdBasedOptimizationSuggestionEngine implements SuggestionEngine {
 	
-	public static final String REWRITE_SELECTOR_WITH_ID_FIRST = "Beginning selectors with an id has the best performance.  Consider re-writing this selector to begin with an id to reduce the size of the DOM to be searched.";
-	public static final String SELECTOR_AND_CHILD_TEMPLATE = "Consider re-writing this selector as $('%s').find('%s') to improve selector performance.";
+	static final String REWRITE_SELECTOR_WITH_ID_FIRST = "Beginning selectors with an id has the best performance.  Consider re-writing this selector to begin with an id to reduce the size of the DOM to be searched.";
+	static final String SELECTOR_AND_CHILD_TEMPLATE = "Consider re-writing this selector as $('%s').find('%s') to improve selector performance.";
 		
 	@Override
 	public Set<String> getSuggestions(String selector) {
--- src/main/java/com/arrested/research/optimization/suggestion/IncreaseSpecificityLeftToRightSuggestionEngine.java
+++ src/main/java/com/arrested/research/optimization/suggestion/IncreaseSpecificityLeftToRightSuggestionEngine.java
@@ -1,13 +1,49 @@
 package com.arrested.research.optimization.suggestion;
 
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
 class IncreaseSpecificityLeftToRightSuggestionEngine implements SuggestionEngine {
 	
+	static final String REWRITE_SUBSELECTOR_LESS_SPECIFIC = "Consider rewriting %s to include only a class or tag.";
+	static final String REWRITE_SUBSELECTOR_MORE_SPECIFIC = "Consider rewriting %s to include both a class and tag.";
+	
 	@Override
 	public Set<String> getSuggestions(String selector) {
-		// TODO Auto-generated method stub
-		return new TreeSet<>();
+		
+		List<List<String>> tokens = SelectorUtils.tokenizeSelector(selector);
+		Set<String> suggestions = new TreeSet<>();
+		
+		for (List<String> selectorTokens : tokens) {
+			
+			if (selectorTokens.size() <= 1) {
+				continue;
+			}
+			
+			String lastToken = "";
+			boolean isComplex = false;
+			
+			for (String token : selectorTokens) {
+				
+				if (isComplexSelector(token)) {
+					isComplex = true; 
+				} else if (isComplex) {
+					suggestions.add(String.format(REWRITE_SUBSELECTOR_LESS_SPECIFIC, lastToken));
+				}
+				
+				lastToken = token;
+			}
+			
+			if (!isComplexSelector(lastToken)) {
+				suggestions.add(String.format(REWRITE_SUBSELECTOR_MORE_SPECIFIC, lastToken));
+			}
+		}
+		
+		return suggestions;
+	}
+	
+	public boolean isComplexSelector(String selector) {
+		return SelectorUtils.isTagSelector(selector) && (selector.indexOf(".") > -1 || selector.indexOf("#") > -1);
 	}
 }
